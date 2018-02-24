c9a890efec44d41d7059fb941a656ca81a06d256
bwegh <baswegh@gmx.de>
2018-02-24 16:15:54 +0100

    fix local session close naming

--- src/ct_router_if_local.erl
+++ src/ct_router_if_local.erl
@@ -7,7 +7,7 @@
          handle_hello_message/2,
          handle_authenticate_message/2,
          handle_established_message/4,
-         handle_session_closed_message/2
+         handle_session_closed/2
         ]).
 
 handle_hello_message(Hello, PeerAtGate) ->
@@ -16,8 +16,8 @@ handle_hello_message(Hello, PeerAtGate) ->
 handle_authenticate_message(Authenticate, PeerAtGate) ->
     ct_router:handle_authenticate(Authenticate, PeerAtGate).
 
-handle_established_message(Type, Message, Session, PeerAtGate) ->
-    ct_router:handle_established(Type, Message, Session, PeerAtGate).
+handle_established_message(Type, Message, SessionId, PeerAtGate) ->
+    ct_router:handle_established(Type, Message, SessionId, PeerAtGate).
 
-handle_session_closed_message(Session, PeerAtGate) ->
-    ct_router:handle_session_closed(Session, PeerAtGate).
+handle_session_closed(SessionId, PeerAtGate) ->
+    ct_router:handle_session_closed(SessionId, PeerAtGate).
